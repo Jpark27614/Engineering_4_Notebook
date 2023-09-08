d80990c67159be9e6feaaebe929939d0c660a9ad
Jpark27614 <Jpark12@charlottesvilleschools.org>
2023-09-08 13:49:48 -0400

    Commenting

--- raspberry-pi/LED_blink.py
+++ raspberry-pi/LED_blink.py
@@ -2,7 +2,7 @@ import board #type:ignore
 import digitalio #type:ignore
 import time #imports the variable time so I can use time.sleep
 import time.sleep #type:ignore
-led = digitalio.DigitalInOut(board.LED) #assigning "led" to mean the LED on the bpard
+led = digitalio.DigitalInOut(board.LED) #assigning "led" to mean the LED on the board
 led.direction = digitalio.Direction.OUTPUT #says LED is an output 
 led.value = True
 
